b0094d3a3b2561dd858427316440c70d9b58ac8a
Tomek <tomasz.machlowski@gmail.com>
2014-06-08 15:45:13 +0200

    Wreszcie dodaje mapkę, z jakimś podstawowym zoomem, oraz trybem pełnoekranowym po wciśnięciu “m”

--- Project/Assets/Player/Scripts/Minimap.cs
+++ Project/Assets/Player/Scripts/Minimap.cs
@@ -2,12 +2,51 @@
 
 public class Minimap : MonoBehaviour 
 {
-
+	
 	public Transform Target;
-
+	public Camera Cam;
+	public bool isFullScreen=false;
+	public Rect camSize;
+	
+	void Start()
+	{
+		camSize = Cam.pixelRect;
+	}
 	void LateUpdate()
 	{
 		transform.position = new Vector3 (Target.position.x, transform.position.y, Target.position.z);
+		
+	}
 	
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.M)) 
+		{
+			if(!isFullScreen)
+			{
+				Cam.orthographicSize+=40;
+				Cam.pixelRect=new Rect(10,10,Screen.width-20,Screen.height-20);
+				isFullScreen=true;
+			}
+			else
+			{
+				Cam.orthographicSize-=40;
+				Cam.pixelRect=camSize;
+				isFullScreen=false;
+			}
+		}
+	}
+	
+	void OnGUI() {
+		if (GUI.Button(new Rect(Screen.width-20, 20, 25, 20), "-")) {
+			Cam.orthographicSize+=20;
+		}
+		if (GUI.Button(new Rect(Screen.width-20, 60, 25, 20), "+")) {
+			if(Cam.orthographicSize>30)
+				Cam.orthographicSize-=20;
+		}
+		//if (GUI.Button(Rect(10,10,50,50),btnTexture)) {
+		//	Debug.Log("Clicked the button!");
+		//}
 	}
 }
