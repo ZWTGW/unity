6449fa43ce6e23af104f38e7857e52f3cffc54e6
mantkowicz <antek15@gmail.com>
2014-06-09 00:43:45 +0200

    flaga - bez networkingu ale działa
    i robi to naprawdę dobrze B)

---
+++ Project/Assets/Player/Scripts/Flag.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flag : MonoBehaviour {
+	
+	public string team; 
+	GameObject keeper = null;
+	bool keepFlag = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (keepFlag) {
+			
+			Vector3 p = keeper.transform.position;
+			
+			transform.position = new Vector3(p.x, p.y, p.z);
+			
+		}
+		
+		if (Input.GetMouseButton(1)) 
+		{
+			keepFlag = false;
+			collider.enabled = true;
+			keeper = null;
+		}
+		
+	}
+	
+	void OnCollisionEnter( Collision col )
+	{
+		if (col.gameObject.tag == "Player") {
+			keepFlag = true;
+			keeper = col.gameObject;
+			collider.enabled = false;
+		}
+	}
+}
---
+++ Project/Assets/Player/Scripts/IgnoreCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+		
+		if (col.gameObject.tag == "Player") {
+			Physics.IgnoreCollision(col.collider, collider);
+		}
+		
+	}
+}
