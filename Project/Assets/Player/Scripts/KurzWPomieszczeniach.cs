c2f61c575b57b0eaf1bc88e5fee33fb16c3207e6
Wacław Jacek <mail@waclawjacek.com>
2014-11-26 09:01:37 +0100

    Dodaje framework "Detonator Explosion Framework", nowa eksplozje granatu, skybox, kurz w pomieszczeniu przy spawnie i mechanizm dodawania kurzu w pomieszczeniach, zmieniam oswietlenie od slonca.

---
+++ Project/Assets/Player/Scripts/KurzWPomieszczeniach.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KurzWPomieszczeniach : MonoBehaviour {
+	public ParticleSystem particleSystem;
+	public Transform[] dustSpawnPointTransforms;
+
+	void Start () {
+		foreach (Transform transform in dustSpawnPointTransforms) {
+			GameObject kurz = Instantiate(particleSystem, transform.position, transform.rotation) as GameObject;
+			Transform kurzTransform = kurz.GetComponent<Transform>();
+			kurzTransform.localScale = transform.localScale;
+		}
+	}
+}
