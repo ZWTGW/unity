d0005833c0cc142814db8b93ddc10fb5411ee41a
Wacław Jacek <mail@waclawjacek.com>
2014-12-11 02:43:31 +0100

    Początki możliwości niszczenia budynków.
    W tej chwili przy spawnie umieszczona jest płaszczyzna, której trafienie (nie zawsze od razu wykrywa trafienie, czasem trzeba strzelić kilka razy) skutkuje zniszczeniem losowej jej części, a następnie pojawienie się sześciennych "ułamków" płaszczyzny.

---
+++ Project/Assets/Scripts/MeshDestruction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshDestruction : MonoBehaviour {
+	public GameObject shardToSpawn;
+	public int amountOfShards = 15;
+
+	void OnCollisionEnter(Collision col) {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		int[] triangles = mesh.triangles;
+		int[] newTriangles = new int[triangles.Length - 3];
+		for (int i = 0; i < triangles.Length - 3; i++) {
+			newTriangles[i] = triangles[i];
+		}
+		mesh.triangles = newTriangles;
+		mesh.RecalculateBounds();
+
+		for (int j = 0; j < 3; j++) {
+			GameObject shard = Instantiate(shardToSpawn, col.collider.transform.position + new Vector3(3, 3, 3), Quaternion.identity) as GameObject;
+			shard.rigidbody.AddForce(Random.value, Random.value, Random.value);
+		}
+	}
+}
