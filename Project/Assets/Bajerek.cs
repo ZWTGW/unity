bbd661856f7b48636fa1e444884fd0b64264966f
pecet <pecet1@gmail.com>
2015-01-08 18:38:42 +0100

    bajerek w menu - wykres spektrum
    nie wiem po co, ale ładnie wygląda, pasuje do tego dubstepu co dał
    Maciek

---
+++ Project/Assets/Bajerek.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// szmerek bajerek do wizualizacji dzwieku w Menu, wyglada fajnie i tyle
+public class Bajerek : MonoBehaviour
+{
+	private AudioSource dzwiek;
+	public float[] s = new float[1024];
+	
+	void Awake ()
+	{
+		this.dzwiek = GetComponent<AudioSource>();
+	}
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		dzwiek.GetSpectrumData(s,0,FFTWindow.Triangle);
+	}
+
+	void OnGUI() {
+		int w = Screen.width;
+		int h = Screen.height;
+		float r = (float)w / 16;
+		float poz = 0;
+
+		// dla tego dubstepa bierzemy tylko poczatek spektrum bo to nie jest normalna muzyka :D
+
+		for(int i = 0; i < 16; i++) {
+			poz = (h - 15) - ((s[i] * 5) * (float)h); // * 5 = też tuningujemy pod konkretna muzyczke ;)
+			GUI.Box(new Rect(i * r, poz, r, h), "");
+		}
+
+	}
+}
--- Project/Assets/Player/Scripts/GraGUI.cs
+++ Project/Assets/Player/Scripts/GraGUI.cs
@@ -472,6 +472,7 @@ public class GraGUI : MonoBehaviour{
 	}
 	
 	void OnGUI() {
+
 		if (isMainMenu) {
 			InGameMenu();
 		}
