270830642a9b5f511886cf726552c3cbee7ed793
Wacław Jacek <mail@waclawjacek.com>
2015-01-14 09:17:03 +0100

    Działające niszczenie budynków! (co prawda czasem błędnie... ale bangla)

--- Project/Assets/Scripts/MeshDestruction.cs
+++ Project/Assets/Scripts/MeshDestruction.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MeshDestruction : MonoBehaviour {
 	public GameObject shardToSpawn;
-	public int amountOfShards = 15;
+	public int amountOfShards = 5;
 
-	void OnCollisionEnter(Collision col) {
-		Mesh mesh = GetComponent<MeshFilter>().mesh;
-		int[] triangles = mesh.triangles;
-		int[] newTriangles = new int[triangles.Length - 3];
-		for (int i = 0; i < triangles.Length - 3; i++) {
-			newTriangles[i] = triangles[i];
-		}
-		mesh.triangles = newTriangles;
-		mesh.RecalculateBounds();
-
-		for (int j = 0; j < 3; j++) {
-			GameObject shard = Instantiate(shardToSpawn, col.collider.transform.position + new Vector3(3, 3, 3), Quaternion.identity) as GameObject;
-			shard.rigidbody.AddForce(Random.value, Random.value, Random.value);
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Ray ray = new Ray(contact.point - contact.normal * 0.05f, contact.normal);
+			RaycastHit hit;
+			if (contact.thisCollider.Raycast(ray, out hit, 100.0F)) {
+				Mesh mesh = hit.collider.GetComponent<MeshFilter>().mesh;
+				List<int> indices = new List<int>(mesh.triangles);
+				indices.RemoveRange(hit.triangleIndex * 3, 3);
+				mesh.triangles = indices.ToArray();
+				mesh.RecalculateBounds();
+				
+				for (int j = 0; j < amountOfShards; j++) {
+					GameObject shard = Instantiate(shardToSpawn, contact.point, Quaternion.identity) as GameObject;
+					shard.rigidbody.AddForce(Random.value, Random.value, Random.value);
+				}
+			}
 		}
 	}
 }
