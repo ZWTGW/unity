0d1e6af1b7d34ed27edd13924c39afdbdbcc3706
pecet <pecet1@gmail.com>
2015-05-04 12:00:05 +0200

    zapis demka do pliku (CZYTAJ KOMITA DALEJ PLOX)
    no więc, klawisze są takie na razie:
    O - nagrywamy demko
    P - odtwarzamy demko ( z pamięci)
    K - zapis do plika
    L - odczyt z plika
    więc możesz sobie coś nagrać wciskając O, później K
    wyłączas projekt, właczasz jeszcze raz i wtedy L, P
    gdzie to się zapisuje?
    do standardowej lokacji unity persistent storage czy jakoś tak, pod
    windowsem:
    c:\Users\TWÓJ USER CZY
    COŚ\AppData\LocalLow\DefaultCompany\Project\demko.demko
    
    i teraz taki myk, bo u mnie chodzi ta gra w ogóle cała, zwłaszcza jak
    nie patrzę na podłogę przez cały czas w 15 fpsach, i teraz nagrywanie
    jest związane z fpsami nagrywającego, więc demka powinien nagrać ktoś
    kto ma glorious 60 fps raczej
    
    po za tym coś nagrywanie rotacji nie działa ale ogarne
    
    z 3 sekundowe demko zajmuje z 40 kb, więc 30 sekundowe będzie zajmowało
    400 kb itp. w sumie myśle że wystarczy na nasze potrzeby
    
    trzeba wywalić sieć, bo jest ona nieużywana a jakieś debilne błędy w
    appwarp.cs wypadają (które to jest od sieci), na razie tego nie robiłem
    bo spawnowanie gracza itp jest w sieci
    
    co z demkami dalej zapytujesz
    otóż, plan jest taki żeby zrobić tak że dodajemy innego gracza i ten
    gracz np. odtwarza demko 1, inny demko 2, a gracz który się rusza 3,
    albo sobie chodzi i udaje że coś robi, na raize odtwarzanie tylko na
    jednym obiekcie player

--- Project/Assets/Player/Scripts/RecordAndPlayDemo.cs
+++ Project/Assets/Player/Scripts/RecordAndPlayDemo.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
 
 
 // pc - taki test w sumie czy można by nagrać demko i np. odtworzyć
@@ -13,8 +15,11 @@ public class RecordAndPlayDemo : MonoBehaviour {
 	bool playing = false;
 	int currentFrame = 0;
 
+	// jak sie tutaj cos doda to format plikow demek oczywiscie nie bedzie kompatybilny ze starym (chyba)
+	[System.Serializable] 
 	struct demoFrame { // klatka dema czy coś
-		public float x, y, z;
+		public float x, y, z; // pozycja
+		public float rw, rx, ry, rz; // rotacja
 	};
 
 	List<demoFrame> demo = new List<demoFrame>();
@@ -32,6 +37,10 @@ public class RecordAndPlayDemo : MonoBehaviour {
 		tmp.x = player.transform.position.x;
 		tmp.y = player.transform.position.y;
 		tmp.z = player.transform.position.z;
+		tmp.rw = player.transform.rotation.w;
+		tmp.rx = player.transform.rotation.x;
+		tmp.ry = player.transform.rotation.y;
+		tmp.rz = player.transform.rotation.z;
 		demo.Add(tmp);
 	}
 
@@ -40,7 +49,7 @@ public class RecordAndPlayDemo : MonoBehaviour {
 		GameObject player = GameObject.FindGameObjectsWithTag ("Player")[0];
 		demoFrame tmp = demo[currentFrame];
 		player.transform.position = new Vector3(tmp.x, tmp.y, tmp.z);
-
+		player.transform.rotation = new Quaternion(tmp.rx, tmp.ry, tmp.rz, tmp.rw);
 
 
 		// zaczynamy od poczatku jak dojdziemy do konca
@@ -60,11 +69,46 @@ public class RecordAndPlayDemo : MonoBehaviour {
 			playing = !playing;
 		}
 
+		if( Input.GetKeyDown (KeyCode.K)) {
+			recording = false;
+			playing = false;
+			SaveToFile();
+		}
+
+		if( Input.GetKeyDown (KeyCode.L)) {
+			recording = false;
+			playing = false;
+			LoadFromFile();
+		}
+
 		if(recording) {
 			recordFrame();
 		}
 		else if(playing) {
 			playFrame();
 		}
+	}
+
+	void SaveToFile(string name = "demko") { // zapisujemy demko
+		string filePath = Application.persistentDataPath + "/" + name + ".demko";
+		if(File.Exists(filePath)) File.Delete(filePath); // wywalamy stary plik i sie o nic nie pytamy (!)
+		BinaryFormatter bf = new BinaryFormatter();
+		Debug.Log ("Zapisujemy demko do: " + filePath);
+		FileStream file = File.Create (filePath);
+		bf.Serialize(file, demo);
+	}
+
+	void LoadFromFile(string name = "demko") {
+		string filePath = Application.persistentDataPath + "/" + name + ".demko";
+		demo.Clear();
+		if(!File.Exists(filePath)) {
+			Debug.Log ("PLIK Z DEMKIEM " + name + " NIE ISTNIEJE!!111111111");
+			return;
+		}
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Open(filePath, FileMode.Open);
+		demo = (List<demoFrame>)bf.Deserialize(file);
+		Debug.Log("Załadowno demko z " + demo.Count + " klatkami"); // jak tu jest 0 a coś mamy to źle raczej
+
 	}
 }
